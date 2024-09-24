b2e2f999fecd275ec7f066f3045e97cd38b51494
Freitas-t <thalles.freitas.teixeira@gmail.com>
2024-09-23 21:39:57 -0300

    Commitando estudos para o repositório

---
+++ condicionais/controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,22 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos em ligações");
+            }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
